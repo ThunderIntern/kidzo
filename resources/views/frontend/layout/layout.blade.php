22321848dc628354b8b97ba9c46ff792f4ce7d45
Mahendra <311310014@student.machung.ac.id>
2016-07-26 14:43:51 +0700

    fix

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -6,11 +6,112 @@
          <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 		{!! Html::style(elixir('css/app.css')) !!}
         <title>@section('title','Welcome')</title>
+        <style>
+            .text{
+                font-family: 'Lato';
+            }
+            .satu{
+                
+                padding-top:25px;
+                float :left;
+                width:17%;
+            }
+            .dua{
+                float :left;
+                width:26%;
+            }
+            .tiga{
+                float :left;
+                width:17%;
+            }
+            .empat{
+                float :left;
+                width:40%;
+            }
+            .a, .b, .c, .d, .e{
+                float :left;
+                width:20%;
+                height:5px;
+            }
+            .foot{
+                padding-left: 50px; 
+                padding-right: 50px;
+                padding-top: 10px;
+            }
+			.textcontent{
+				padding-left: 10px;
+			}
+			.home, .mainan, .party, .tentang{
+                border-top: 5px solid;
+                border-right: 1px solid #ebebe0;
+            }
+            .login{
+                border-top: 5px solid;
+            }
+            .fa{
+                padding-top: 15px;
+            }
+
+            @media (max-width: 768px){
+                .dekstop{
+                    display: none;
+                }
+                .normala, .normalb{
+                    display:none;
+                }
+                .kecila{
+                    padding-top: 50px;
+                    padding-bottom: 50px;
+                }
+                .kecilb{
+                    text-align: center;
+                    padding-bottom: 15px;
+                    padding-top: 15px;
+                }
+                .email{
+                    border-bottom-left-radius: 5px;
+                    border-top-left-radius: 5px;
+                    border:1px solid #E2E2E2;
+                    padding:5px 10px;
+                }
+                button{
+                    border-bottom-right-radius: 5px;
+                    border-top-right-radius: 5px;
+                    border:1px solid #E2E2E2;
+                    padding:5px 10px;
+                    margin-left: -5px;
+                }
+            }
+
+            @media(min-width: 992px){
+                .mobile{
+                    display: none;
+                }
+                .kecila, .kecilb{
+                    display:none;
+                }
+                .normala{
+                    padding-top: 30px;
+                    padding-bottom: 170px;
+                }
+                .email{
+                    border-bottom-left-radius: 5px;
+                    border-top-left-radius: 5px;
+                    border:1px solid #E2E2E2;
+                    padding:5px 10px;
+                }
+                button{
+                    border-bottom-right-radius: 5px;
+                    border-top-right-radius: 5px;
+                    border:1px solid #E2E2E2;
+                    padding:5px 10px;
+                    margin-left: -5px;
+                }
+            }
+		</style>
 	</head>
     <body>
-		@include('frontend.widget.header')
 		@yield('content')
-		@include('frontend.widget.footer')
     </body>
     {!! Html::script(elixir('js/app.js')) !!}
 </html>
--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -1,131 +1,86 @@
 	@extends('frontend.layout.layout')
 	@section('content')
 	<div class="dekstop">
-		<img width="100%" class="img-responsive img-center" src="image/capture1.jpg"></img>	
+		<img width="100%" class="img-responsive img-center" src="{{asset('assets/image/capture1.jpg')}}"></img>	
 	</div>
 	<div class="mobile">
-		<img width="100%" class="img-responsive img-center" src="image/capture1.jpg"></img>	
+		<img width="100%" class="img-responsive img-center" src="{{asset('assets/image/capture1.jpg')}}"></img>	
 	</div>
-	<br><br>
-	<div class="container-fluid">
-		<div class="col-sm-12 text-center">
+	<div class="container-fluid mtop">
+		<div class="col-sm-12 col-md-12 text-center">
 			<h2>3 Langkah Mudah Sewa Mainan di Kidzo</h2>
 			<br><br>
 		</div>
-		<div class="col-sm-4 text-center">
-			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="image/capture2.jpg"></img><br><br></a>
+		<div class="col-sm-4 col-md-4 text-center">
+			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="{{asset('assets/image/capture2.jpg')}}"></img><br><br></a>
 			<h2>1. Browse</h2><br>
 			<h3>Pilih produk permainan yang Anda suka.</h3>
 		</div>
-		<div class="col-sm-4 text-center">
-			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="image/capture2.jpg"></img><br><br></a>
+		<div class="col-sm-4 col-md-4 text-center">
+			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="{{asset('assets/image/capture2.jpg')}}"></img><br><br></a>
 			<h2>2. Browse</h2><br>
 			<h3>Pilih produk permainan yang Anda suka.</h3>
 		</div>
-		<div class="col-sm-4 text-center">
-			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="image/capture2.jpg"></img><br><br></a>
+		<div class="col-sm-4 col-md-4 text-center">
+			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="{{asset('assets/image/capture2.jpg')}}"></img><br><br></a>
 			<h2>3. Browse</h2><br>
 			<h3>Pilih produk permainan yang Anda suka.</h3>
 		</div>
-		<div class="col-sm-12 text-center">
+		<div class="col-md-12 col-sm-12 text-center mtop">
 			<br><br>
 			<h2>Kategori Permainan</h2>
 			<hr class="garis-bawah" width="100">
 			<br><br>
 		</div>
-		<div class="dekstop">
-			<div class="col-sm-2"></div>
-			<a href="#"><div class="col-sm-2 bg0 kotak-left">
+			<div class="col-sm-12 col-md-2"></div>
+			<a href="#"><div class="col-sm-12 col-md-2 bg0 kotak">
 				<h1 class="text-center green">0 - 1<br>tahun</h1>
 			</div></a>
-			<a href="#"><div class="col-sm-2 bg3 kotak">
+			<a href="#"><div class="col-sm-12 col-md-2 bg3 kotak">
 				<h1 class="text-center orange">1 - 2<br>tahun</h1>
 			</div></a>
-			<a href="#"><div class="col-sm-2 bg1 kotak">
+			<a href="#"><div class="col-sm-12 col-md-2 bg1 kotak">
 				<h1 class="text-center purple">2 - 3<br>tahun</h1>
 			</div></a>
-			<a href="#"><div class="col-sm-2 bg2 kotak-right">
+			<a href="#"><div class="col-sm-12 col-md-2 bg2 kotak">
 				<h1 class="text-center aqua">3 +<br>tahun</h1>
 			</div></a>
-			<div class="col-sm-2"></div>
-		</div>
-		<div class="mobile">
-			<a href="#"><div class="col-sm-12 bg0 kotak-mobile">
-				<h1 class="text-center green">0 - 1<br>tahun</h1>
-			</div></a>
-			<a href="#"><div class="col-sm-12 bg3 kotak-mobile">
-				<h1 class="text-center orange">1 - 2<br>tahun</h1>
-			</div></a>
-			<a href="#"><div class="col-sm-12 bg1 kotak-mobile">
-				<h1 class="text-center purple">2 - 3<br>tahun</h1>
-			</div></a>
-			<a href="#"><div class="col-sm-12 bg2 kotak-mobile">
-				<h1 class="text-center aqua">3 +<br>tahun</h1>
-			</div></a>
-		</div>
+			<div class="col-sm-12 col-md-2"></div>
 	</div>
-	<br><br><br><br>
-	<div class="bgblue dekstop">
+	<div class="bgblue mtop">
 		<div class="container-fluid">
-			<div class="col-sm-8">
+			<div class="col-sm-12 col-md-8">
 				<h2 class="white ptop">Party Time</h2>
-				<h3 class="white ptop pbottom">
+				<h3 class="white ptop pbottom font-mobile">
 					wfhasbucgbasiuvgbicsuabvicuabsuivcbiausbv iuasbvuiabiuvbiaubvisuabviubasiuvuiasbvib asiuviubasviuasibviuasbviuabsivbiabvibais ucvyudsgvgwugvbuidviuvbiusbviubiuvbiubviu ewbvuebwuvbwuibfaggfauygufaygfugasuygfyua sguyfgasggfugsaufygsaugfuyahsbfhsabfjhbsa jhbfjshsjhfjhsjgsjhfgsjgsjhgfjgjhgsjfgjsa</h3>
 				<a href="#"><button class="btn-lg bgwhite blue mbottom">Lebih Lanjut</button></a>
 			</div>
-			<div class="col-sm-4">
-				<a href="#"><img class="img-responsive img-center img-m-top" src="image/capture3.jpg"></img></a>
+			<div class="col-md-4">
+				<a href="#"><img class="img-responsive img-center img-m-top" src="{{asset('assets/image/capture3.jpg')}}"></img></a>
 			</div>
 		</div>
 	</div>
-	<div class="bgblue mobile">
-		<div class="container-fluid"  width="100%">
-			<div class="col-sm-12">
-				<h2 class="white ptop">Party Time</h2>
-				<h3 class="white font-mobile ptop pbottom">
-					wfhasbucgbasiuvgbicsuabvicuabsuivcbiausbv iuasbvuiabiuvbiaubvisuabviubasiuvuiasbvib asiuviubasviuasibviuasbviuabsivbiabvibais ucvyudsgvgwugvbuidviuvbiusbviubiuvbiubviu ewbvuebwuvbwuibfaggfauygufaygfugasuygfyua sguyfgasggfugsaufygsaugfuyahsbfhsabfjhbsa jhbfjshsjhfjhsjgsjhfgsjgsjhgfjgjhgsjfgjsa</h3>
-				<a href="#"><button class="btn-lg bgwhite blue mbottom">Lebih Lanjut</button></a>
-			</div>
-		</div>
-	</div>
-	<br><br><br><br>
-	<div class="container-fluid">
-		<div class="col-sm-12 text-center">
+	<div class="container-fluid mtop">
+		<div class="col-sm-12 col-md-12 text-center">
 			<h2>Mainan Terbaru</h2>
 			<hr class="garis-bawah" width="100">
 			<br><br>
 		</div>
-		<div class="dekstop">
-			<div class="col-sm-2"></div>
-			<div class="col-sm-2 kotak-left">
-				@include('frontend.widget.truck')
-			</div>
-			@for($i=0;$i<2;$i++)
-			<div class="col-sm-2 kotak">
-				@include('frontend.widget.truck')
-			</div>
-			@endfor
-			<div class="col-sm-2 kotak-right">
-				@include('frontend.widget.truck')
-			</div>
-			<div class="col-sm-2"></div>
-		</div>
-		<div class="mobile">
+			<div class="col-sm-12 col-md-2"></div>
 			@for($i=0;$i<4;$i++)
-			<div class="col-sm-12">
+			<div class="col-sm-12 col-md-2 kotak">
 				@include('frontend.widget.truck')
 			</div>
 			@endfor
+			<div class="col-sm-12 col-md-2"></div>
+	</div>
+	<div class="container-fluid mbottom mtop">
+		<div class="col-sm-12 text-center col-md-6">
+			<h2 class="blue"><b>Temukan <a class="orange">1900</a> Koleksi Mainan Lainnya !</b></h2>
 		</div>
-	</div>
-	<br><br>
-	<div class="container dekstop mbottom">
-		<h2 class="blue"><b class="mleft mright">Temukan <a class="orange">1900</a> Koleksi Mainan Lainnya !</b><a href="#"><button class="text-right btn btn-primary btn-lg white">Lihat Koleksi</button></a></h2>
-	</div>
-	<div class="col-sm-12 mobile text-center mbottom">
-		<h2 class="blue font-mobile" width="100%"><b>Temukan <a class="orange">1900</a> Koleksi Mainan Lainnya !<br></b></h2>
-	</div>
-	<div class="col-sm-12 mobile text-center mbottom">
-		<a href="#"><button class="btn btn-primary btn-lg white">Lihat Koleksi</button></a>
+		<div class="col-md-3"></div>
+		<div class="col-sm-12 col-md-3">
+			<a href="#"><button class="text-center btn btn-primary btn-lg white">Lihat Koleksi</button></a>
+		</div>
 	</div>
     @stop
