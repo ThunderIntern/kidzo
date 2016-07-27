c4a493f3670265267a5fef5fa630d04ce9a60426
Mahendra <311310014@student.machung.ac.id>
2016-07-27 15:12:13 +0700

    content homepage fix

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -1,83 +1,106 @@
 	@extends('frontend.layout.layout')
 	@section('content')
-	<div>
+	<div class="mbottom">
 		<img width="100%" class="img-responsive img-center" src="{{asset('image/frontend/frontend/capture1.jpg')}}"></img>	
 	</div>
-	<div class="container-fluid mtop mbottom">
-		<div class="col-sm-12 col-md-12 text-center">
-			<h2>3 Langkah Mudah Sewa Mainan di Kidzo</h2>
-			<br><br>
-		</div>
-		<div class="col-sm-4 col-md-4 text-center">
-			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="{{asset('image/frontend/frontend/capture2.jpg')}}"></img><br><br></a>
-			<h2>1. Browse</h2><br>
-			<h3>Pilih produk permainan yang Anda suka.</h3>
-		</div>
-		<div class="col-sm-4 col-md-4 text-center">
-			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="{{asset('image/frontend/frontend/capture2.jpg')}}"></img><br><br></a>
-			<h2>2. Browse</h2><br>
-			<h3>Pilih produk permainan yang Anda suka.</h3>
-		</div>
-		<div class="col-sm-4 col-md-4 text-center">
-			<a href="#"><img width="250" class="img-responsive img-circle img-center" src="{{asset('image/frontend/frontend/capture2.jpg')}}""></img><br><br></a>
-			<h2>3. Browse</h2><br>
-			<h3>Pilih produk permainan yang Anda suka.</h3>
-		</div>
-		<div class="col-md-12 col-sm-12 text-center mtop mbottom">
-			<br><br>
-			<h2>Kategori Permainan</h2>
-			<hr class="garis-bawah" width="100">
-			<br><br>
-		</div>
-			<div class="col-sm-12 col-md-2"></div>
-			<a href="#"><div class="col-sm-12 col-md-2 bg0 kotak">
-				<h1 class="text-center green">0 - 1<br>tahun</h1>
-			</div></a>
-			<a href="#"><div class="col-sm-12 col-md-2 bg3 kotak">
-				<h1 class="text-center orange">1 - 2<br>tahun</h1>
-			</div></a>
-			<a href="#"><div class="col-sm-12 col-md-2 bg1 kotak">
-				<h1 class="text-center purple">2 - 3<br>tahun</h1>
-			</div></a>
-			<a href="#"><div class="col-sm-12 col-md-2 bg2 kotak">
-				<h1 class="text-center aqua">3 +<br>tahun</h1>
-			</div></a>
-			<div class="col-sm-12 col-md-2"></div>
-	</div>
-	<div class="bgblue mtop">
-		<div class="container-fluid">
-			<div class="col-sm-12 col-md-8">
-				<h2 class="white ptop">Party Time</h2>
-				<h3 class="white ptop pbottom font-mobile">
-					wfhasbucgbasiuvgbicsuabvicuabsuivcbiausbv iuasbvuiabiuvbiaubvisuabviubasiuvuiasbvib asiuviubasviuasibviuasbviuabsivbiabvibais ucvyudsgvgwugvbuidviuvbiusbviubiuvbiubviu ewbvuebwuvbwuibfaggfauygufaygfugasuygfyua sguyfgasggfugsaufygsaugfuyahsbfhsabfjhbsa jhbfjshsjhfjhsjgsjhfgsjgsjhgfjgjhgsjfgjsa</h3>
-				<a href="#"><button class="btn-lg bgwhite blue mbottom">Lebih Lanjut</button></a>
-			</div>
-			<div class="col-md-4 desktop">
-				<a href="#"><img class="img-responsive img-center img-m-top pbottom" src="{{asset('image/frontend/frontend/capture3.jpg')}}"></img></a>
+	<div class="container pleft-l pright-l">
+		<div class="row mbottom">
+			<div class="col-sm-12 col-md-12 text-center">
+				<h2>3 Langkah Mudah Sewa Mainan di Kidzo</h2>
 			</div>
 		</div>
-	</div>
-	<div class="container-fluid mtop">
-		<div class="col-sm-12 col-md-12 text-center">
-			<h2>Mainan Terbaru</h2>
-			<hr class="garis-bawah" width="100">
-			<br><br>
-		</div>
-			<div class="col-sm-12 col-md-2"></div>
-			@for($i=0;$i<4;$i++)
-			<div class="col-sm-12 col-md-2 kotak">
-				@include('frontend.widget.truck')
+		<div class="row mbottom">
+			<div class="col-sm-12 col-md-4 text-center">
+				<a href="#"><img width="100%" class="img-responsive img-circle img-center" src="{{asset('image/frontend/frontend/capture2.jpg')}}"></img><br><br></a>
+				<h2>1. Browse</h2><br>
+				<h3>Pilih produk permainan yang Anda suka.</h3>
 			</div>
-			@endfor
-			<div class="col-sm-12 col-md-2"></div>
-	</div>
-	<div class="container-fluid mbottom mtop">
-		<div class="col-sm-12 col-md-6 text-center">
-			<h2 class="blue"><b>Temukan <a class="orange">1900</a> Koleksi Mainan Lainnya !</b></h2>
+			<div class="col-sm-12 col-md-4 text-center">
+				<a href="#"><img width="100%" class="img-responsive img-circle img-center" src="{{asset('image/frontend/frontend/capture2.jpg')}}"></img><br><br></a>
+				<h2>2. Browse</h2><br>
+				<h3>Pilih produk permainan yang Anda suka.</h3>
+			</div>
+			<div class="col-sm-12 col-md-4 text-center">
+				<a href="#"><img width="100%" class="img-responsive img-circle img-center" src="{{asset('image/frontend/frontend/capture2.jpg')}}""></img><br><br></a>
+				<h2>3. Browse</h2><br>
+				<h3>Pilih produk permainan yang Anda suka.</h3>
+			</div>
 		</div>
-		<div class="col-md-2"></div>
-		<div class="col-sm-12 col-md-4 text-center">
-			<a href="#"><button class="btn btn-primary btn-lg white">Lihat Koleksi</button></a>
+		<div class="row mbottom">
+			<div class="col-md-12 col-sm-12 text-center">
+				<h2>Kategori Permainan</h2>
+				<hr class="garis-bawah" width="100">
+			</div>
+		</div>
+		<div class="row mbottom">
+			<div class="col-md-12">
+				<div class="col-sm-12 col-md-3 text-center">
+					<div class="row">
+						<div class="col-12 kotak padding">
+							<a href="#"><h1 class="green bg0 kotakKategori">0 - 1<br>tahun</h1></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-3 text-center">
+					<div class="row">
+						<div class="col-12 kotak padding">
+							<a href="#"><h1 class="orange bg3 kotakKategori">1 - 2<br>tahun</h1></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-3 text-center">
+					<div class="row">
+						<div class="col-12 kotak padding">
+							<a href="#"><h1 class="purple bg1 kotakKategori">2 - 3<br>tahun</h1></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-3 text-center">
+					<div class="row">
+						<div class="col-12 kotak padding">
+							<a href="#"><h1 class="aqua bg2 kotakKategori">3 + <br>tahun</h1></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="bgblue mbottom">
+			<div class="container pleft-l pright-l">
+				<div class="row">
+					<div class="col-sm-12 col-md-8">
+						<h2 class="white ptop">Party Time</h2>
+						<h3 class="white ptop pbottom font-mobile">
+							wfhasbucgbasiuvgbicsuabvicuabsuivcbiausbv iuasbvuiabiuvbiaubvisuabviubasiuvuiasbvib asiuviubasviuasibviuasbviuabsivbiabvibais ucvyudsgvgwugvbuidviuvbiusbviubiuvbiubviu ewbvuebwuvbwuibfaggfauygufaygfugasuygfyua sguyfgasggfugsaufygsaugfuyahsbfhsabfjhbsa jhbfjshsjhfjhsjgsjhfgsjgsjhgfjgjhgsjfgjsa</h3>
+						<a href="#"><button class="btn-lg bgwhite blue mbutton-bottom">Lebih Lanjut</button></a>
+					</div>
+					<div class="col-md-4 desktop">
+						<a href="#"><img width="100%" class="img-responsive img-m-top img-center" src="{{asset('image/frontend/frontend/capture3.jpg')}}"></img></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	<div class="container pleft-l pright-l">
+		<div class="row mbottom">
+			<div class="col-sm-12 col-md-12 text-center">
+				<h2>Mainan Terbaru</h2>
+				<hr class="garis-bawah" width="100">
+			</div>
+		</div>
+		<div class="row mbottom">
+			<div class="col-md-12">
+				@for($i=0;$i<4;$i++)
+				<div class="col-sm-12 col-md-3">
+					@include('frontend.widget.truck')
+				</div>
+				@endfor
+			</div>
+		</div>
+		<div class="row mbottom">
+			<div class="col-sm-12 col-md-12">
+				<h2 class="blue"><b>Temukan <a class="orange">1900</a> Koleksi Mainan Lainnya !</b>
+				<a href="#"><button class="btn btn-primary btn-lg white pull-right">Lihat Koleksi</button></a></h2>
+			</div>
 		</div>
 	</div>
     @stop
--- resources/views/frontend/widget/truck.blade.php
+++ resources/views/frontend/widget/truck.blade.php
@@ -1,3 +1,3 @@
-<a href="#"><img width="250" class="img-responsive img-center" src="{{asset('image/frontend/frontend/capture4.jpg')}}"></img>
-<h3 class=" blue textcontent"><br>Strawberry Truck</h3>
-<h5 class="textcontent black"><i>from</i> Rp. 123k Available</h5></a>
+<a href="#"><img width="100%" class="img-responsive img-center" src="{{asset('image/frontend/frontend/capture4.jpg')}}"></img>
+<h3 class=" blue"><br>Strawberry Truck</h3>
+<h4 class="black"><i>from</i> Rp. 123k Available</h4></a>
