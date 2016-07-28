7dd407fca0a419bcb9df32d416c9f1490210571e
budi7 <budi-purnomo@outlook.com>
2016-07-28 14:42:02 +0700

    init backend styling & page - backend scss - layout - dashboard page

---
+++ resources/views/backend/layout/modal.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fullscreen-modal" id='modal_api'>
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div class="row">
+					<div class="col-xs-12 text-xs-right">
+						<a href="#" data-dismiss='modal' class='text-primary'>
+							<span class="fa-stack fa-lg">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-close fa-stack-1x fa-inverse"></i>
+							</span>
+						</a>
+					</div>
+				</div>
+
+				<div class='content'></div>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
---
+++ resources/views/backend/widgets/topbar.blade.php
@@ -0,0 +1,41 @@
+<!-- top liner -->
+<nav class="navbar navbar-dark navbar-fixed-top navbar-full bg-primary bg-faded main-nav">
+	<a class="navbar-brand" href="#">KIDZO.ID</a>
+	<div class="nav navbar-nav">
+		<a href="http://cms.capcus.id/logout" class="pull-xs-right nav-link"><i class="fa fa-sign-out"></i> Logout
+		</a>
+	</div>
+</nav>
+
+<!-- second liner -->
+<nav class="navbar navbar-fixed-top navbar-full bg-faded navbar2">
+	<div class="nav navbar-nav">
+		<ul class="list-inline">
+			<li class="list-inline-item text-xs-center">
+				<a href="#" class="block p-y-1 p-x-0 ">
+					<i class="fa fa-dashboard fa-2x fa-fw"></i><br>Dashboard
+				</a>
+			</li>
+			<li class="list-inline-item text-xs-center">
+				<a href="#" class="block p-y-1 p-x-0 ">
+					<i class="fa fa-plane fa-2x fa-fw"></i><br>Trips
+				</a>
+			</li>
+			<li class="list-inline-item text-xs-center">
+				<a href="#" class="block p-y-1 p-x-0 ">
+					<i class="fa fa-newspaper-o fa-2x fa-fw"></i><br>Contents
+				</a>
+			</li>
+			<li class="list-inline-item text-xs-center">
+				<a href="#" class="block p-y-1 p-x-0 ">
+					<i class="fa fa-file-text-o fa-2x fa-fw"></i><br>Reports
+				</a>
+			</li>
+			<li class="list-inline-item text-xs-center">
+				<a href="#" class="block p-y-1 p-x-0 ">
+					<i class="fa fa-user fa-2x fa-fw"></i><br>Me
+				</a>
+			</li>
+		</ul>
+	</div>
+</nav>
