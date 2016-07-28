0607ba1844e459335817b7fbfbe8b841318e97ea
budi7 <budi-purnomo@outlook.com>
2016-07-28 16:16:02 +0700

    fixed base layout page website backend

---
+++ resources/views/backend/pages/website.blade.php
@@ -0,0 +1,29 @@
+@extends('backend.layout.layout')
+@section('content')
+<div class="container-fluid">
+	<div class="row clearfix">
+		&nbsp;
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12 col-lg-3">
+		@include('backend.widgets.sidebar', ['components' => [
+			'0' => ['link' => "#", 'caption' => 'Config'],
+			'1' => ['link' => "#", 'caption' => 'Sliders'],
+			'2' => ['link' => "#", 'caption' => 'FAQ'],
+		]]);
+		</div>
+ 
+		<div class="col-xs-12 col-lg-9">
+			<div class="row mb-s">
+				<div class="col-sm-12">
+					<div class="row">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@stop
