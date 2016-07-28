b09d217b897d6d405e7890056e3e5dd8dcdd01f8
budi7 <budi-purnomo@outlook.com>
2016-07-28 16:15:21 +0700

    fixed base layout crm page backend

---
+++ resources/views/backend/pages/crm.blade.php
@@ -0,0 +1,28 @@
+@extends('backend.layout.layout')
+@section('content')
+<div class="container-fluid">
+	<div class="row clearfix">
+		&nbsp;
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12 col-lg-3">
+		@include('backend.widgets.sidebar', ['components' => [
+			'0' => ['link' => "#", 'caption' => 'Subscribber'],
+			'1' => ['link' => "#", 'caption' => 'Newsletter'],
+		]]);
+		</div>
+ 
+		<div class="col-xs-12 col-lg-9">
+			<div class="row mb-s">
+				<div class="col-sm-12">
+					<div class="row">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@stop
