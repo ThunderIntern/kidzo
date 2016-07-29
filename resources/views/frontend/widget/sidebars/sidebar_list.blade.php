5daf6760632134af4f441953a8303905b78f7d50
mahendra <311310014@student.machung.ac.id>
2016-07-29 09:11:29 +0700

    frontend about fix

--- resources/views/frontend/widget/sidebars/sidebar_list.blade.php
+++ resources/views/frontend/widget/sidebars/sidebar_list.blade.php
@@ -1,2 +1,2 @@
-<h6 class="pul mbottom-s"><b>{!!$data['header']!!}</b></h6>
-	<a href="{!!$data['link']!!}"><li class="pil black mbottom-s"><h6>{!!$data['content']!!}</h6></li></a>
+<h6 class="pul mbottom-xs"><b>{!!$data['header']!!}</b></h6>
+	<a href="{!!$data['link']!!}"><li class="pil black mbottom-xs"><h6>{!!$data['content']!!}</h6></li></a>
