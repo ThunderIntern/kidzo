70f429bf89626c0e820bf8522746c78d53e23d0a
budi7 <budi-purnomo@outlook.com>
2016-08-08 16:14:30 +0700

    fixed backend slider index

--- resources/views/backend/pages/website/slider/index.blade.php
+++ resources/views/backend/pages/website/slider/index.blade.php
@@ -8,35 +8,56 @@
 		'link-search'	=> '#',
 	]])
 	</div>
+	@include('backend.widgets.alertbox')
 	<div class="card-block">
 		<div class="table-responsive">
 			<table class="table table-hover">
 				<thead>
 					<tr>
 						<th class="col-md-1">#</th>
-						<th class="col-md-9">Published At</th>
-						<th class="col-md-2">Edited By</th>
+						<th class="col-md-5">Published At</th>
+						<th class="col-md-4">Published By</th>
+						<th class="col-md-2 text-xs-right">Control</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td class="col-md-1">1</td>
-						<td class="col-md-9"><a href="{{Route('backend.website.slider.show',['id' => '1'])}}">01-Aug-2016</td>
-						<td class="col-md-2">Admin</td>
-					</tr>
-					<tr>
-						<td class="col-md-1">2</td>
-						<td class="col-md-9">01-Aug-2016</td>
-						<td class="col-md-2">Admin</td>
-					</tr>
-					<tr>
-						<td class="col-md-1">3</td>
-						<td class="col-md-9">01-Aug-2016</td>
-						<td class="col-md-2">Admin</td>
-					</tr>										
+					@forelse ($page_datas->datas as $key => $data)
+						<tr>
+							<td class="col-md-1">
+								{{($page_datas->datas->perpage() * ($page_datas->datas->currentPage()-1)) + ($key + 1)}}
+							</td>
+							<td class="col-md-5">
+								<a href="{{route('backend.website.slider.show', ['id' => $data['id']])}}">
+									{!! $data['published_at']->format('d M Y h:i A') !!}
+								</a>
+							</td>
+							<td class="col-md-4">
+								{{ucfirst($data['admin'])}}
+							</td>
+							<td class="col-md-2 text-xs-right">
+								<a href="{{route('backend.website.slider.edit', ['id' => $data['id']])}}" class="btn btn-primary-outline btn-sm">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+						        </a>	
+								<a href="#" class="btn btn-primary-outline btn-sm" data-toggle="modal" data-target="#modalDelete" data-action="{!! route('backend.website.slider.destroy',['id' => $data['id']]) !!}">
+									<i class="fa fa-times" aria-hidden="true"></i>
+						        </a>
+							</td>
+						</tr>										
+					@empty
+						<tr>
+							<td COLSPAN=4>
+								Tidak ada data
+							</td>
+						</tr>
+					@endforelse
 				</tbody>
 			</table>
 		</div>
+		{!! $page_datas->datas->render(new \Illuminate\Pagination\BootstrapFourPresenter($page_datas->datas)); !!}
 	</div>
 </div>
 @stop
+
+@section('modal')
+	@include('backend.modals.delete')
+@append
