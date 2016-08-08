343ea0a492683899b903db5ea9d350f6b1d04bb4
budi7 <budi-purnomo@outlook.com>
2016-08-08 16:14:45 +0700

    working backend slider controller

--- app/Http/Controllers/Backend/Website/sliderController.php
+++ app/Http/Controllers/Backend/Website/sliderController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Backend\Website;
 
 use App\Http\Controllers\baseController;
-use Request;
+
+use App\Models\WebsiteConfig;
+use App\Models\Version;
+use Request, Input, URL;
+
 
 class sliderController extends BaseController
 {
-    protected $view_source_root             = 'backend.pages.website.slider';
-    protected $page_title                   = 'website';
-    protected $breadcrumb                   = [];
+    protected $view_source_root                 = 'backend.pages.website.slider';
+    protected $page_title                       = 'Slider';
+    protected $breadcrumb                       = [];
     public function __construct()
     {
         parent::__construct();
@@ -23,12 +27,19 @@ class sliderController extends BaseController
      */
     public function index()
     {
+        //get data
+        $WebsiteConfig                          = new WebsiteConfig;
+        $datas                                  = $WebsiteConfig->paginate(10);
+
+        $this->page_datas->datas                = $datas;
+        $this->page_datas->id                   = null;
+
         //page attributes
-        $this->page_attributes->page_title  = $this->page_title;
+        $this->page_attributes->page_title      = $this->page_title;
 
         //generate view
-        $view_source                       = $this->view_source_root . '.index';
-        $route_source                      = Request::route()->getName();        
+        $view_source                            = $this->view_source_root . '.index';
+        $route_source                           = Request::route()->getName();        
         return $this->generateView($view_source , $route_source);
     }
 
@@ -37,9 +48,35 @@ class sliderController extends BaseController
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($id = null)
     {
-        //
+        $datas                                  = null;
+
+        if($id != null)
+        {
+            //get data
+            $WebsiteConfig                      = new WebsiteConfig;
+            $datas                              = $WebsiteConfig->find($id);
+        }
+
+        //set data
+        $this->page_datas->datas                = $datas;
+
+        //set referral url
+        $this->setRefererUrl();
+
+        //page attributes
+        if($id != null){
+            $this->page_attributes->page_title  = 'Edit '. $this->page_title;
+        }else{
+            $this->page_attributes->page_title  = $this->page_title . ' Baru';
+        }
+        $this->page_datas->id                   = $id;
+
+        //generate view
+        $view_source                            = $this->view_source_root . '.create';
+        $route_source                           = Request::route()->getName();        
+        return $this->generateView($view_source , $route_source);
     }
 
     /**
@@ -48,9 +85,9 @@ class sliderController extends BaseController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store($id = null)
     {
-        //
+
     }
 
     /**
@@ -61,7 +98,21 @@ class sliderController extends BaseController
      */
     public function show($id)
     {
-        //
+        //get data
+        $WebsiteConfig                          = new WebsiteConfig;
+        $datas                                  = $WebsiteConfig::find($id);
+
+        $this->page_datas->datas                = $datas;
+        $this->page_datas->id                   = $id;
+
+        //page attributes
+        $this->page_attributes->page_title      = 'Detail ' . $this->page_title;
+
+
+        //generate view
+        $view_source                            = $this->view_source_root . '.show';
+        $route_source                           = Request::route()->getName();        
+        return $this->generateView($view_source , $route_source);
     }
 
     /**
@@ -72,7 +123,7 @@ class sliderController extends BaseController
      */
     public function edit($id)
     {
-        //
+        return $this->create($id);
     }
 
     /**
@@ -82,9 +133,10 @@ class sliderController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update($id)
     {
-        //
+        return $this->store($id);
+        
     }
 
     /**
@@ -95,6 +147,23 @@ class sliderController extends BaseController
      */
     public function destroy($id)
     {
-        //
+        //find 
+        $WebsiteConfig                          = WebsiteConfig::find($id);
+
+        //get password
+        $password                               = Input::get('password');
+        if(empty($password)){
+            $this->errors                       = "Password not valid";
+        }else{
+            //delete data
+            $WebsiteConfig->delete();
+            
+            $this->errors                       = $WebsiteConfig->getErrors();
+        }
+
+        //return view
+        $this->page_attributes->msg             = 'Data telah dihapus';
+
+        return $this->generateRedirect(route('backend.website.slider.index'));
     }
 }
