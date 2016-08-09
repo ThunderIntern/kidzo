cfc170615717b289bfb40b5d37ebfcc8299961f0
mahendra <311310014@student.machung.ac.id>
2016-08-09 14:10:15 +0700

    Registered Unsubscribed Routing Fix

--- resources/views/frontend/pages/unsubscribed.blade.php
+++ resources/views/frontend/pages/unsubscribed.blade.php
@@ -3,7 +3,7 @@
     <div class="container berlangganan modal-dialog text-center">
         <div class="col-md-12 col-sm-12 font-registered">
             Anda Telah Berhasil Berhenti Berlangganan<br>Terima Kasih Sudah Mendaftar Newsletter Kidzo<br>
-            <a href="http://localhost:8000/home"><button class="btn btn-success">OK</button></a>
+            <a href="{{Route('home')}}"><button class="btn btn-success">OK</button></a>
         </div>
     </div>
 @stop
