4fbb0fe8069e33cf6505452d0eeea7c145438d9d
budi7 <budi-purnomo@outlook.com>
2016-08-10 13:19:37 +0700

    patch component detail blade handle null data

--- resources/views/backend/widgets/components/detail/detail-pusblishedDate.blade.php
+++ resources/views/backend/widgets/components/detail/detail-pusblishedDate.blade.php
@@ -3,7 +3,7 @@
 		<span class="text-uppercase"><strong>{!! $component['title'] !!}</strong></span>
 	</div>
 	<div class="col-xs-9">
-		{!! $component['content'] ? $component['content']->format('d M Y h:i A') : '_' !!}
+		{!! $component['content'] ? $component['content']->format('d M Y h:i A') : 'Not Published Yet' !!}
 	</div>		
 	<div class="col-xs-12"><hr></div>
 </div>
