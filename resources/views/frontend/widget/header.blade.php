3ef193e63aa890c3928818fc571861ef5e6587b0
gray gevaldi <311310010@student.machung.ac.id>
2016-08-13 13:59:50 +0800

    navbar fix

--- resources/views/frontend/widget/header.blade.php
+++ resources/views/frontend/widget/header.blade.php
@@ -26,23 +26,24 @@ button:
                             <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#tes">
                                 &#9776;                                                 
                             </button>
+                            <?php $segment1 =  Request::segment(1);  ?>
                             <div class="collapse navbar-toggleable-xs" id="tes">
                                 <ul class="nav navbar-nav">
                                     <li class="nav-item borderTop5 green"></li>
-                                    <li class="nav-item borderTop5 red paddingBottom25  paddingTop25">
-                                        <a class="marginRight-10 paddingRight10 black paddingBottom20 paddingTop20 borderRight1" href="{{Route('home')}}">Home</a>
+                                    <li class="homeHover nav-item borderTop5 red paddingBottom25  paddingTop25">
+                                        <a class="marginRight-10 paddingRight10 black paddingBottom20 paddingTop20 borderRight1 <?php if($segment1=='home') echo 'red';?>" href="{{Route('home')}}">Home</a>
                                    </li>
-                                    <li class="nav-item borderTop5 green paddingBottom25 paddingTop25">
-                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black borderRight1" href="#">Mainan</a>
+                                    <li class="mainanHover nav-item borderTop5 green paddingBottom25 paddingTop25">
+                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black borderRight1 <?php if($segment1=='mainan') echo 'green';?>" href="#">Mainan</a>
                                     </li>
-                                    <li class="nav-item borderTop5 orange paddingBottom25 paddingTop25">
-                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black borderRight1" href="#">Party Pack</a>
+                                    <li class="partyHover nav-item borderTop5 orange paddingBottom25 paddingTop25">
+                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black borderRight1 <?php if($segment1=='party') echo 'orange';?>" href="#">Party Pack</a>
                                     </li>
-                                    <li class="nav-item borderTop5 purple paddingBottom25 paddingTop25">
-                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black borderRight1" href="{{Route('about')}}">Tentang Kami</a>
+                                    <li class="tentangHover nav-item borderTop5 purple paddingBottom25 paddingTop25">
+                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black borderRight1 <?php if($segment1=='about') echo 'purple';?>" href="{{Route('about')}}">Tentang Kami</a>
                                     </li>
-                                    <li class="nav-item borderTop5 blue paddingBottom25 paddingTop25">
-                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black" href="#">Login</a>
+                                    <li class="loginHover nav-item borderTop5 blue paddingBottom25 paddingTop25">
+                                        <a class="marginRight-10 paddingRight10 paddingBottom20 paddingTop20 black <?php if($segment1=='login') echo 'blue';?>" href="#">Login</a>
                                     </li>
                                 </ul>
                             </div>
@@ -88,79 +89,71 @@ button:
                             <button class="navbar-toggler padding30 dropbtn" type="button">
                                 <a href="#"><i class="fa fa-shopping-cart fa-lg paddingRight10 black" aria-hidden="true"></i></a>
                             </button>
-                            {{-- Button navbar --}}
-                            <button class="navbar-toggler padding10" type="button" data-toggle="modal" data-target="#myModal2">
-                                &#9776;
-                            </button>
+
+
+                            <div class="dropdown">  {{-- Tombol search --}}
+                                <a href="#">
+                                    <button onclick="myFunction3()" class="black dropbtn navbar-toggler padding30" type="button" data-toggle="collapse">
+                                        &#9776;
+                                    </button>
+                                </a>                                
+                            </div> {{-- Tombol search --}}
                         </div>
                     </div>
-                </nav>
-                <!-- Modal -->
-                <div class="modal right fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                {{-- Button cancel --}}
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></br></br>
-                                {{-- Button shoping + search --}}
-                                <div class="icon pull-right">
-                                    <div class="dropdown text-xs-right">
-                                    {{-- Button Shopping --}}
-                                    <button class="navbar-toggler dropbtn" type="button">
-                                        <a href="#"><i class="dropbtn black fa fa-shopping-cart fa-lg" aria-hidden="true"></i></a>
-                                    </button>
-
-                                    {{-- Button search --}}
-                                        <a href="#">
-                                            <button onclick="myFunction2()" class="dropbtn navbar-toggler" type="button">
-                                                <i class="dropbtn fa fa-search fa-lg black" aria-hidden="true"></i>
-                                            </button>
-                                        </a>
-                                    {{-- Isi Button Search --}}
-                                        <div id="myDropdown2" class="dropdown-cont box">
-                                            <form class="form-inline">
-                                                    {!! Form::text('email_mobile',null, ['class' => 'form-control width80Per floatLeft', 'placeholder'=>'Cari Produk']) !!}
-                                                    <button type="submit" class="black bgabu width20Per butFoot floatLeft">Cari</button>
-                                            </form></br>
-                                            <a href="#" class="textCenter paddingTop10 paddingBottom10">Advance Search</a>
-                                        </div>
-                                    </div>
+                    {{-- Yang keluar saat tombol search ditekan --}}
+                    <div id="myDropdown3" class="dropdown-content box marginTop77"> 
+                                    <form class="form-inline marginTop15 paddingLeft30 paddingRight30">
+                                            {!! Form::text('email_mobile',null, ['class' => 'form-control width80Per floatLeft', 'placeholder'=>'Cari Mainan']) !!}
+                                            <button type="submit" class="black bgabu width20Per butFoot floatLeft"><i class="fa fa-search fa-lg black" aria-hidden="true"></i></button>
+                                        </br></br><button type="submit" class="buttonAdvan width100Per">
+                                        <a href="#" class="textCenter">
+                                            <div style="color:#808080">Pencarian Lanjutan</div>
+                                        </a></button>
+                                    </form>
+                                    </br></br>
+                                     <ul class="nav navbar-nav">
+                                        <li class="nav-item borderTop5">
+                                        </li>
+                                        <li class="nav-item hoverHome">
+                                            <a class="nav-link pull-left paddingLeft30" href="{{Route('home')}}">
+                                                <div class="red">Home</div>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item hoverMainan ">
+                                            <a class="nav-link pull-left paddingLeft30" href="#">
+                                                <div class="green">Mainan</div>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item hoverParty ">
+                                            <a class="nav-link pull-left paddingLeft30" href="#">
+                                                <div class="orange">Party Package</div>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item hoverTentang ">
+                                            <a class="nav-link pull-left paddingLeft30" href="{{Route('about')}}">
+                                                <div class="purple">Tentang Kami</div>
+                                            </a>
+                                        </li>
+                                        <li class="nav-item hoverLogin">
+                                            <a class="nav-link pull-left paddingLeft30" href="#">
+                                                <div class="blue">Login</div>
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
-                            </div>
-
-                            <div class="modal-body">
-                                <ul class="nav navbar-nav">
-                                    <li class="nav-item borderTop5 green">
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link black pull-right" href="{{Route('home')}}">Home</a>
-                                    </li>
-                                    <li class="nav-item borderTop1 green">
-                                        <a class="nav-link black pull-right" href="#">Mainan</a>
-                                    </li>
-                                    <li class="nav-item borderTop1 orange">
-                                        <a class="nav-link black pull-right" href="#">Party Pack</a>
-                                    </li>
-                                    <li class="nav-item borderTop1 purple">
-                                        <a class="nav-link black pull-right" href="{{Route('about')}}">Tentang Kami</a>
-                                    </li>
-                                    <li class="nav-item borderTop1 blue">
-                                        <a class="nav-link black pull-right" href="#">Login</a>
-                                    </li></br></br></br></br></br></br></br></br></br></br></br></br></br>
-                                </ul>
-                            </div>
-                        </div><!-- modal-content -->
-                    </div><!-- modal-dialog -->
-                </div><!-- modal -->
+                </nav>
             </div>{{-- Saat ukuran mobile --}}
             
             
 <script>
-    function myFunction() {
+function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 function myFunction2() {
     document.getElementById("myDropdown2").classList.toggle("show");
+}
+function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
