d3c13f0c6cd8a768f6a012ed1453f30c075eb204
gray gevaldi <311310010@student.machung.ac.id>
2016-10-08 21:49:45 +0800

    repair statistic .blade

--- resources/views/backend/pages/crm/statistic/noData.blade.php
+++ resources/views/backend/pages/crm/statistic/noData.blade.php
@@ -3,12 +3,16 @@
 <div class="card">
 	<div class="card-block">
 		<div class="col-md-12">
-		</br></br></br></br>
+			<h1 class="text-center mtop-s">Data Statistik</h1>
+			<p><i>(Sistem ini menghitung berdasarkan data setiap bulan dalam 1 tahun, sehingga pada bulan pertama-ketiga hasil perhitungan tidak akan optimal. Perhitungan untuk mendapatkan hasil yang optimal memerlukan setidaknya history transaksi minimal sebanyak 3 bulan. Sistem akan memproses dan menghasilkan nilai yang optimal untuk jenis mainan dengan permintaan tertinggi pada bulan kemarin.)</i></p></br>
+		</div>
+		<div class="col-md-12">
+		
 			@if($page_datas->divZero == 0)
 				<h3 style="text-align:center">Tidak ada data di dalam database</h3>
 			@endif
 			@if($page_datas->divZero == 1)
-				<h3 style="text-align:center">Adanya pembagian dengan nol, coba lagi pada bulan berikutnya</h3>
+				<h5 style="text-align:center">Terdapat pembagian dengan nol karena kurang banyaknya variasi data, coba lagi pada bulan berikutnya</h5>
 			@endif
 		</br></br></br></br>
 		</div>
